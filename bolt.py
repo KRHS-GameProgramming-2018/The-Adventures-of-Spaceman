424b1df7bd028cc10ba020b7cacea3c51fe3478b
peter russell <peter.russell@kearsarge.org>
2018-12-20 08:25:24 -0500

    bullet stuff

--- bolt.py
+++ bolt.py
@@ -1,3 +1,7 @@
 import sys, math, pygame
 from mob import *
 from player import *
+
+class Bolt(Mob):
+    def __init__(self, speed=10, startPos=[0,0]):
+        Mob.__init__(self, "PNG/Player/spaceman.png", [0,0], startPos)
