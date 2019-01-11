0a5124af66832d34ae5b2f816d3919961bf3640c
Aaron Thayer <acthayer01@gmail.com>
2019-01-11 08:25:23 -0500

    Added images to imposter.py

--- Imposter.py
+++ Imposter.py
@@ -1,1 +1,22 @@
 import pygame, sys, math, random
+from mob import *
+
+class SpaceZombie(Mob):
+    def __init__(self, speed=1, startPos=[0,0]):
+        Mob.__init__(self, "PNG/Enemy2/Imposter-Down.png", [0,0], startPos)
+        self.rightImages = [pygame.image.load("PNG/Enemy2/Imposter-Right1.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Right2.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Right3.png"),
+                        ]
+        self.leftImages = [pygame.image.load("PNG/Enemy2/Imposter-Left1.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Left2.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Left3.png"),
+                        ]
+        self.upImages = [pygame.image.load("PNG/Enemy2/Imposter-Up.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Up.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Up.png"),
+                        ]
+        self.downImages = [pygame.image.load("PNG/Enemy2/Imposter-Down.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Down.png"),
+                        pygame.image.load("PNG/Enemy2/Imposter-Down.png"),
+                        ]
