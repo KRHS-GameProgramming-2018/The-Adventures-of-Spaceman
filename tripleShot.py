1c16e308ca8a999d40d35300f926d9561bafdd63
peter russell <peter.russell@kearsarge.org>
2019-02-06 07:51:39 -0500

    tripleshot start

--- tripleShot.py
+++ tripleShot.py
@@ -1,1 +1,20 @@
+import sys, math, pygame
+from mob import *
+from player import *
+
+class Tripleshot(Mob):
+    def __init__(self, image="PNG/Bolt/bolt-1.png", speed=0, startPos=[0,0]):
+        Mob.__init__(self,  image, speed, startPos)
+        # ~ print self.rect.center, speed
+        self.kind = "tripleshot"
+        self.lives = 1
+        
+    def collide(self, other):
+        if self.rect.right > other.rect.left:
+            if self.rect.left < other.rect.right:
+                if self.rect.top < other.rect.bottom:
+                    if self.rect.bottom > other.rect.top:
+                        if self.radius + other.radius > self.getDist(other.rect.center):
+                            if othre.type="player"
+        return False
 
