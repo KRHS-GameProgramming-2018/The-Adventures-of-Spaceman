c998e46fd5f0e9132f5d76143a3fb9f62263751d
Aaron Thayer <acthayer01@gmail.com>
2019-04-16 08:25:27 -0400

    Interaction with merchant pauses game now

--- main.py
+++ main.py
@@ -121,15 +121,18 @@ while True:
                                     if event.type == pygame.KEYDOWN:
                                         if event.key == pygame.K_t:
                                             paused = False
-                        #if event.key == pygame.K_e:
-                            #if in merchant.radius:
-                                #paused = True
-                                #while paused:
-                                    #for event in pygame.event.get():
-                                        #if event.type == pygame.QUIT: sys.exit()
-                                        #if event.type == pygame.KEYDOWN:
-                                            #if event.key == pygame.K_t:
-                                                #paused = False
+                    ###~PLAYER MERCHANT INERACTION~###
+                        if event.key == pygame.K_e:
+                            for mob in mobs:
+                                if mob.kind == "merchant":
+                                    if mob.checkPlayer(pb.rect.center):
+                                        paused = True
+                                        while paused:
+                                            for event in pygame.event.get():
+                                                if event.type == pygame.QUIT: sys.exit()
+                                                if event.type == pygame.KEYDOWN:
+                                                    if event.key == pygame.K_e:
+                                                        paused = False
                         #for facing directions
                         if event.key == pygame.K_UP:
                             pb.face("face up")
