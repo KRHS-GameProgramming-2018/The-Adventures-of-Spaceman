51567127a78b0b01d799fc4a903681e23b505ede
peter russell <peter.russell@kearsarge.org>
2019-04-18 07:54:38 -0400

    all fixes for demo day

--- HUD.py
+++ HUD.py
@@ -4,14 +4,15 @@ import pygame, sys, math
 #HealthBar and Power Ups 
 class GameDisplay(pygame.sprite.Sprite):
     def __init__(self, size, bullets, lives):
-        self.images = [pygame.image.load("PNG/backgrounds/spacemansheart.png"),([175,975]),
-                       pygame.image.load("PNG/backgrounds/spacemansheart2.png"),([175,975]),
-                       pygame.image.load("PNG/backgrounds/spacemansheart3.png"),([175,975]),
-                       pygame.image.load("PNG/backgrounds/spacemansheart4.png"),([175,975]),
-                       pygame.image.load("PNG/backgrounds/spacemansheart5.png"),([175,975]),
-                       pygame.image.load("PNG/backgrounds/spacemansheart6.png"),([175,975])]
+        self.images = [pygame.image.load("PNG/backgrounds/spacemansheart.png"),
+                       pygame.image.load("PNG/backgrounds/spacemansheart2.png"),
+                       pygame.image.load("PNG/backgrounds/spacemansheart3.png"),
+                       pygame.image.load("PNG/backgrounds/spacemansheart4.png"),
+                       pygame.image.load("PNG/backgrounds/spacemansheart5.png"),
+                       pygame.image.load("PNG/backgrounds/spacemansheart6.png")
+                       ]
         self.image = self.images[lives]
-        self.rect = self.image.get_rect(center=(175,975))
+        self.rect = self.image.get_rect(center=[175,975])
         
     def update(*args):
         self = args[0]
@@ -19,6 +20,6 @@ class GameDisplay(pygame.sprite.Sprite):
         lives = args[3]
         bullets = args[4]
         
-        self.image = self.images[health]
+        self.image = self.images[lives]
         self.rect = self.image.get_rect()
         pass
