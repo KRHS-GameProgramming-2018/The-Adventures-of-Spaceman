c106cb0437af6d6e10b563c72b41b766147fc038
peter russell <peter.russell@kearsarge.org>
2019-04-19 07:44:56 -0400

    hud

--- HUD.py
+++ HUD.py
@@ -5,15 +5,17 @@ import pygame, sys, math
 class GameDisplay(pygame.sprite.Sprite):
     def __init__(self, size, bullets, lives):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.images = [pygame.image.load("PNG/backgrounds/spacemansheart.png"),
+        self.healthImages = [pygame.image.load("PNG/backgrounds/spacemansheart.png"),
                        pygame.image.load("PNG/backgrounds/spacemansheart2.png"),
                        pygame.image.load("PNG/backgrounds/spacemansheart3.png"),
                        pygame.image.load("PNG/backgrounds/spacemansheart4.png"),
                        pygame.image.load("PNG/backgrounds/spacemansheart5.png"),
                        pygame.image.load("PNG/backgrounds/spacemansheart6.png")
                        ]
-        self.image = self.images[lives]
+        self.healthImage = self.healthImages[lives]
         self.rect = self.image.get_rect(center=[175,775])
+        
+        
         
     def update(*args):
         self = args[0]
@@ -23,6 +25,9 @@ class GameDisplay(pygame.sprite.Sprite):
         
         if lives > 6:
             lives = 6
-        self.image = self.images[lives-1]
+        self.HealthImage = self.HealthImages[lives-1]
+        if bullets > 20:
+            bullets = 20
+        self.BulletImage = self.BulletImages[bullets-1]
 
         pass
