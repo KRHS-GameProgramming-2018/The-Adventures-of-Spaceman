6ba3f9b0be3ab10df4423497bd94b721df146712
jcardillo123 <cardillojake@outlook.com>
2019-05-23 08:22:10 -0400

    fonts

--- CoinCounter.py
+++ CoinCounter.py
@@ -8,7 +8,7 @@ class CoinCounter(pygame.sprite.Sprite):
     def __init__(self,  coins=0, pos = [0,0]):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.coin = coins
-        self.font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font("8-Bit Madness.ttf", 48)
         # ~ self.shellImage = pygame.image.load("PNG/Power-ups/c0.png")
         # ~ self.shellRect = self.image.get_rect(center = [980, 150])
         self.image = self.font.render(str(self.coin), True, (255,255,255))
